41b03009063af12ca2cc1945c03138fbc9f825a0
angelprogramador <angelrodriguezcastro29@gmail.com>
2024-04-25 17:32:18 -0300

    actializando errores

--- javascrip/js.js
+++ javascrip/js.js
@@ -4,51 +4,62 @@ const total = document.querySelector("#total");
 
 function convertir() {
     let valore = parseInt(document.getElementById("monto").value);
+    
+    // Verificar si el valor ingresado es un número válido
+    if (isNaN(valore) || valore <= 0) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Debes ingresar un monto válido",
+        });
+        return; // Salir de la función si el monto no es válido
+    }
+
     let resultado = 0;
     let dolar = 1025;
     let euro = 925.20;
-    if (document.getElementById("dolar").checked){
+    if (document.getElementById("dolar").checked) {
         resultado = valore / dolar;
         montoConvertido = resultado.toFixed(2);
-        total.textContent = `$${montoConvertido}`; 
-        let modal = document.getElementById("myModal");
-        let modalContent = document.getElementById("convertedPrice");
-        modalContent.innerHTML = `El cambio de pesos a dólares es: ${montoConvertido}`;
-        modal.style.display = "block";
-    }
-    else if (document.getElementById("euro").checked){
+        total.textContent = `$${montoConvertido}`;
+        const cotizador = document.querySelector("#cotizador");
+
+        cotizador.addEventListener("click", () => {
+            Swal.fire({
+                title: "Su cambio es: ",
+                text: montoConvertido,
+                icon: "success"
+            });
+        });
+    } else if (document.getElementById("euro").checked) {
         resultado = valore / euro;
         montoConvertido = resultado.toFixed(2);
         total.textContent = `$${montoConvertido}`;
-        let modal = document.getElementById("myModal");
-        let modalContent = document.getElementById("convertedPrice");
-        modalContent.innerHTML = `El cambio de pesos a euros es: ${montoConvertido}`;
-        modal.style.display = "block";
+        Swal.fire({
+            title: "Su cambio es: ",
+            text: montoConvertido,
+            icon: "success"
+        });
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Debes seleccionar una moneda",
+        });
     }
-    else{
-        let modal = document.getElementById("myModal")
-        let modalContent = document.getElementById("convertedPrice");
-
-       
-        modalContent.innerHTML = "Tienes que completar todos los requerimientos";
-        modal.style.display = "block";
-    }
-  
-
 }
 
 let closeButton = document.getElementsByClassName("close")[0];
 
-closeButton.onclick = function() {
+closeButton.onclick = function () {
     let modal = document.getElementById("myModal");
     modal.style.display = "none";
 }
 
-
 function Lista(producto, color, precio) {
     this.producto = producto;
     this.color = color;
-    this.precio = precio; 
+    this.precio = precio;
 }
 
 const Lista1 = new Lista("carpa", "verde bordo", 70000);
@@ -57,10 +68,10 @@ const Lista3 = [
     "mesa", "comedor", "televisor", "microondas", "cocina", "fernet con coca"
 ];
 
-for (angel = 0; angel < 2; angel++ ) {
+for (angel = 0; angel < 2; angel++) {
     console.log(Lista3);
 }
-for ( js = 0; js < 3; js++ ) {
+for (js = 0; js < 3; js++) {
     console.log(Lista3[5]);
 }
 for (viper = 0; viper < 6; viper++) {
@@ -83,35 +94,24 @@ const autos = {
 
 localStorage.setItem("autos", JSON.stringify(autos))
 
-const botonDarkMode = document.querySelector("#Dark");
-const body = document.body;
-let darkMode = localStorage.getItem("dark-mode")
+function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+}
+window.addEventListener('load', addDarkmodeWidget);
 
-function activarModoOscuro() {
-    body.classList.add("dark-mode");
-    localStorage.setItem("dark-mode", "activado");
+const options = {
+    bottom: '64px', // default: '32px'
+    right: 'unset', // default: '32px'
+    left: '32px', // default: 'unset'
+    time: '0.5s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: false, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
 }
 
-function desactivarModoOscuro() {
-    body.classList.remove("dark-mode");
-    localStorage.setItem("dark-mode", "desactivado")
-}
-
-if (darkMode === "activado") {
-    activarModoOscuro();
-} else {
-    desactivarModoOscuro();
-}
-
-botonDarkMode.addEventListener("click", () => {
-    darkMode = localStorage.getItem("dark-mode");
-    
-    if (darkMode === "activado") {
-        desactivarModoOscuro();
-    } else {
-        activarModoOscuro()
-    }
-
-})
-
-
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
